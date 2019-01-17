2ab9468f361f828007ffe035d6abfce6f74b3460
gera1013 <gera1013@users.noreply.github.com>
2019-01-16 20:39:50 -0600

    Changes
    Los objetos con jerarquía rotan en el aire, el cubo puede hacerse más grande y más pequeño con las teclas SHIFT de ambos lados.

---
+++ MendezGerardo_Laboratorio2/Assets/Scripts/Agrandar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agrandar : MonoBehaviour
+{
+    int mas = 7;
+    int menos = -7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector3 = new Vector3(1, 1, 1);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+            transform.localScale += new Vector3(Time.deltaTime * mas, Time.deltaTime * mas, Time.deltaTime * mas);
+        else if(Input.GetKey(KeyCode.RightShift))
+            transform.localScale += new Vector3(Time.deltaTime * menos, Time.deltaTime * menos, Time.deltaTime * menos);
+    }
+}
